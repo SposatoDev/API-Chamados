fd850e3400613ac54eff0b9e4287a8698f3570d0
Kauã Sato Sposato <kaua.sato@gmail.com>
2026-09-19 11:57:12 -0300

    Configuração e conexão banco de dados

---
+++ src/main/java/com/faculdade/chamados/chamadosApplication.java
@@ -0,0 +1,12 @@
+package com.faculdade.chamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class chamadosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(chamadosApplication.class, args);
+    }
+}
